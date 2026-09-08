239fa0dd85eb6e850822498d3625ee39dc1598d2
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-09 02:22:16 +0300

    Load profile and settings polish patch

--- build.js
+++ build.js
@@ -8,9 +8,10 @@ const core=write('app.core',fs.readFileSync(path.join(root,'content.js'),'utf8')
 const runtime=write('app.runtime',fs.readFileSync(path.join(root,'runtime.js'),'utf8'));
 const ux=write('app.ux',fs.readFileSync(path.join(root,'ux-patch.js'),'utf8'));
 const pro=write('app.pro',fs.readFileSync(path.join(root,'pro-ui.js'),'utf8'));
+const settings=write('app.settings',fs.readFileSync(path.join(root,'settings-patch.js'),'utf8'));
 let html=fs.readFileSync(indexPath,'utf8');
 html=html.replace(/<script[^>]*src=["'][^"']+["'][^>]*><\/script>/g,'');
 html=html.replace(/<script>[^]*?<\/script>/g,'');
-html=html.replace('</body>',`<script defer src="${core}"></script><script defer src="${runtime}"></script><script defer src="${ux}"></script><script defer src="${pro}"></script></body>`);
+html=html.replace('</body>',`<script defer src="${core}"></script><script defer src="${runtime}"></script><script defer src="${ux}"></script><script defer src="${pro}"></script><script defer src="${settings}"></script></body>`);
 fs.writeFileSync(indexPath,html);
-console.log('KPSS-Mobil lightweight build: '+core+' + '+runtime+' + '+ux+' + '+pro);
+console.log('KPSS-Mobil lightweight build: '+core+' + '+runtime+' + '+ux+' + '+pro+' + '+settings);
